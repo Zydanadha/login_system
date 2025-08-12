c48e726136331787eb14f2a276273ab67e51470c
zydanadha <zydanadharizkulloh>
2025-08-12 15:48:55 +0700

    update file login

--- login.php
+++ login.php
@@ -141,6 +141,8 @@ p {
     <form action="" method="POST">
         <label for="username">Username:</label>
         <input type="text" name="username" required><br>
+        <label for="username">telepone:</label>
+        <input type="text" name="telepone" required><br>
         <label for="password">Password:</label>
         <input type="password" name="password" required><br>
         <button type="submit">Login</button>
